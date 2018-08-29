a23700cd7e6b5330a23ad39e2a7c0293bff526e7
Vanoux <vanessa.roux891@orange.fr>
2018-08-29 22:35:00 +0200

    Debugg implementation films echec + ajout indentifiants db

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,7 +11,8 @@
  * file.
  */
 
-return [
+
+/* return [
     'db' => [
         'driver' => 'Pdo',
         'dsn' => 'mysql:dbname=zend; host=den1.mysql5.gear.host',
@@ -20,5 +21,18 @@ return [
         ),
         'username' => 'zend',
         'password' => 'Eb47m~p4VSi~',
+    ],
+];
+ */
+// indentifiants DB
+return [
+    'db' => [
+        'driver' => 'Pdo',
+        'dsn' => 'mysql:dbname=rentmovies; host=den1.mysql2.gear.host',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ),
+        'username' => 'rentmovies',
+        'password' => 'Gn2oZ4~1GCv?',
     ],
 ];
